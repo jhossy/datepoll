490a6043f372758ce24df8a6fe83da7012fa2750
hos <hos@lnx-laptop.lan>
2026-02-08 13:11:57 +0100

    initial commit

---
+++ src/components/CreatePoll.vue
@@ -0,0 +1,25 @@
+<script setup>
+    function doCreatePoll() {
+        alert('Submitted poll');
+    }
+</script>
+
+<template>
+    <div>
+        <h1>Create Poll</h1>
+        <div>
+            <h2>1. Give it a name</h2>
+            <input type="text" placeholder="Enter name here..."><input>
+        </div>
+        <div>
+            <h2>2. Select date & time</h2>
+            <input type="datetime-local" />
+        </div>
+        <div>
+            <h2>3. Create poll</h2>
+            <button @click="doCreatePoll">Submit</button>
+        </div>
+    </div>
+</template>
+
+
