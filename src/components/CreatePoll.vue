05d5f1d9e77ab0835a827e2dbd94a48484613339
hos <hos@lnx-laptop.lan>
2026-02-08 15:52:55 +0100

    initial add functionality

--- src/components/CreatePoll.vue
+++ src/components/CreatePoll.vue
@@ -3,14 +3,17 @@
 
     const date = new Date().toISOString().slice(0, 16);
     const currentDate = ref(date);
-    var selectedDates = [];
+    const selectedDates = ref([]);
 
     function doCreatePoll() {
         alert('Submitted poll with id: ' + crypto.randomUUID());
     }
 
     function doAddDate(){
-        selectedDates.push(currentDate);
+        if(!selectedDates.value.includes(currentDate.value)) {
+            selectedDates.value.push(currentDate.value);
+            console.log('added - ' + currentDate.value);
+        }
         //alert('Clicked add date with ' + currentDate.value);
         for(var i = 0; i < selectedDates.length; i++){
             console.log(selectedDates[i].value);
@@ -29,6 +32,11 @@
             <h2>2. Add dates & time</h2>
             <input type="datetime-local" v-model="currentDate" />
             <button @click="doAddDate">Add date</button>
+        </div>
+        <div>
+            <div v-for="(date, index) in selectedDates">
+                <input type="text" key="index" v-model="selectedDates[index]" />
+            </div>
         </div>
         <div>
             <h2>3. Create poll</h2>
