6855839115d45a414caa0c7271509c0156256643
jhossy <hossy@godmail.dk>
2026-02-10 22:04:35 +0100

    Update CreatePoll.vue

--- src/components/CreatePoll.vue
+++ src/components/CreatePoll.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { computed, ref } from 'vue';
+    import { computed, readonly, ref } from 'vue';
 
     const date = new Date().toISOString().slice(0, 16);
     const description = ref('');
@@ -32,7 +32,7 @@
 </script>
 
 <template>
-    <div>
+    <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
         <h1>Create new datepoll</h1>
         <div class="mb-3">
             <label for="lbl-enter-name" class="form-label" aria-label="Give it a name">1. Give it a name</label>
@@ -43,12 +43,12 @@
         <div class="mb-3">
             <label for="lbl-add-date-and-time" class="form-label" aria-label="Add dates & time">2. Add dates & time</label>
             <div class="input-group">                
-                <input v-model="currentDate" id="lbl-add-date-and-time" class="form-control" type="datetime-local" aria-label="Current date"/>
+                <input v-model="currentDate" id="lbl-add-date-and-time" class="form-control mx-1" type="datetime-local" aria-label="Current date"/>
                 <button @click="doAddDate" :disabled="isDisabled" type="button" class="btn btn-success" aria-label="Add date">Add date</button>
                 <div v-for="(date, index) in selectedDates" :key="index" class="input-group">
                     <div class="input-group">
-                        <input v-model="selectedDates[index]" type="text" class="form-control m-1"/>
-                        <button @click="doRemove(index)" type="button" class="btn btn-danger m-1">x</button>
+                         <input :value="new Date(date).toLocaleString()" disabled type="text" class="form-control m-1" />
+                        <button @click="doRemove(index)" type="button" class="btn btn-danger m-1">x</button>                        
                     </div>
                 </div>
             </div>            
@@ -56,7 +56,7 @@
         <div class="mb-3">
             <label for="lbl-create-poll" class="form-label" aria-label="Create poll">3. Create poll</label>
             <div class="input-group">   
-                <button @click="doCreatePoll" type="button" class="btn btn-success m-1">Create poll</button>
+                <button @click="doCreatePoll" type="button" class="btn btn-success">Create poll</button>
             </div>
         </div>
     </div>
